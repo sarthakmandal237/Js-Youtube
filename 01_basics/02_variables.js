bd734b3879689b2141b0e7f7d1a01eb6ffa1a59a
Sarthak Mandal <158606652+sarthakmandal237@users.noreply.github.com>
2025-02-16 14:43:02 +0000

    container config done

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using nodejs not browser
+console.log(3
+    +
+    3)//code readability should be high 
+console.log("Sarthak")
+
+let name="sarthak"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>" "
+// boolean => true/false
+// null => standalonbe value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
