d2bab2606c4490336afacbcfacaeeb1601bc3af3
Sarthak Mandal <158606652+sarthakmandal237@users.noreply.github.com>
2025-02-16 20:06:06 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,45 @@
+const name="sarthak"
+const repoCount=50
+
+// console.log(name+repoCount+" Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String("Sarthak-sm-com")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-9,4)
+console.log(anotherString);
+
+const newString1="   sarthak   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url="https://sarthak.com%20mandal"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+ 
+
+
+
+
+
+
+
+
+
