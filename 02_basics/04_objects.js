dcffad26a3ad36a0061516597fb0da4a0c3cb9bd
Sarthak Mandal <158606652+sarthakmandal237@users.noreply.github.com>
2025-02-17 14:23:14 +0000

    objects part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -37,12 +37,35 @@ const users=[
     },
 ]
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"sarthak"
+}
+
+// course.courseInstructor
+const{courseInstructor: instructor}=course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name":"raj",
+//     "coursename":"Js in Hindi",
+//     "price":"free"
+// }
+[
+    {},
+    {},
+    {}
+]
 
 
 
