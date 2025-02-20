c948d130dc95cf21299f767c6cbaa5782e44a5e9
Sarthak Mandal <158606652+sarthakmandal237@users.noreply.github.com>
2025-02-20 18:31:23 +0000

    high order array loops in js

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject={
+    js:'javascript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'Swift by apple'
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const prog=["js", "ruby", "python", "java"]
+
+for (const key in prog) {
+    //console.log(prog[key]);
+    
+}
+
+// const map=new Map()
+// map.set(`IN`,"India")
+// map.set(`USA`,"United States of America")
+// map.set(`Fr`,"France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
